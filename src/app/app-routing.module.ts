557797231d8e31f88b56ea397c9f59ae3f3d5f67
Gabriel Paredes <gparedes165@gmail.com>
2021-03-11 17:01:53 -0500

    CAMBIOS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,25 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+
+
+  {
+    path: 'inicio',
+    loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
+  },
+  {
+    path: 'agendarcita',
+    loadChildren: () => import('./pages/agendarcita/agendarcita.module').then( m => m.AgendarcitaPageModule)
+  },
+  
 ];
 
 @NgModule({
---
+++ src/app/pages/agendarcita/agendarcita.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-agendarcita',
+  templateUrl: './agendarcita.page.html',
+  styleUrls: ['./agendarcita.page.scss'],
+})
+export class AgendarcitaPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
